309826bbb6241a1ba47909721b1f25ad00f9cc85
Spencer Stolworthy <sdstolworthy@gmail.com>
2021-05-05 17:04:43 -0600

    update for jenkins pipeline

--- infra-deployment.sh
+++ infra-deployment.sh
@@ -2,14 +2,15 @@
 #!/bin/sh
 # This is a comment!
 echo Hello World        # This is a comment, too!
+echo $NAMESPACE
 
-Oc login 
+oc login 
 
-oc new-project poc-dev
+oc new-project $NAMESPACE
 
-oc process openshift//jenkins-ephemeral | oc apply -f- -n poc-dev
+oc process openshift//jenkins-ephemeral | oc apply -f- -n $NAMESPACE
 
-oc set env dc/jenkins JENKINS_JAVA_OVERRIDES=-Dhudson.model.DirectoryBrowserSupport.CSP='' INSTALL_PLUGINS=ansicolor:0.5.2 -n poc-dev
+oc set env dc/jenkins JENKINS_JAVA_OVERRIDES=-Dhudson.model.DirectoryBrowserSupport.CSP='' INSTALL_PLUGINS=ansicolor:0.5.2 -n $NAMESPACE
 
 
-oc process -f applier/templates/infra-build.yaml  | oc apply -f- -n poc-dev
+oc process -f applier/templates/infra-build.yaml  | oc apply -f- -n $NAMESPACE
